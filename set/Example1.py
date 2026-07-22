020630f33ea73b9afe3caeb28528cde337a76f7c
Rajendra Samal <rajendrakumar13863@gmail.com>
2026-07-22 12:38:29 +0530

    Added

---
+++ set/Example1.py
@@ -0,0 +1,10 @@
+collection = {1,2,3,4}
+
+print(collection)
+print(type(collection))
+
+collection1 = {1,2,4,"world", "world", 2,5}
+print(collection1)
+
+collection2 = set() #empty
+print(type(collection2))
