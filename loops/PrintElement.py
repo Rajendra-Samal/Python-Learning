85ccc5234a76d5effcd5a970c988a59d309bf66e
Rajendra Samal <rajendrakumar13863@gmail.com>
2026-07-23 11:26:07 +0530

    Added

---
+++ loops/PrintElement.py
@@ -0,0 +1,18 @@
+elements = [10, 20, 30, 40, 50]
+
+for x in elements:
+    print(x)
+
+    elements1 = [10, 20, 30, 40, 50]
+
+for i in range(len(elements1)):
+    print(i, elements1[i])  # index and value
+
+
+#using while loop
+    elements2 = [10, 20, 30, 40, 50]
+
+i = 0
+while i < len(elements2):
+    print(elements2[i])
+    i += 1
