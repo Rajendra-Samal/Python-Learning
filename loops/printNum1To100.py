3d18d78d2c302a8a8d5beaa8606c3a41d2cd6b9e
Rajendra Samal <rajendrakumar13863@gmail.com>
2026-07-25 10:39:46 +0530

    Added

---
+++ loops/printNum1To100.py
@@ -0,0 +1,18 @@
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+    # Using a for loop (most common)
+for i in range(1, 101):
+    print(i)
+
+    # 100 to 1
+    # Using a for loop
+for i in range(100, 0, -1):
+    print(i)
+
+    i = 100
+while i >= 1:
+    print(i)
+    i -= 1
