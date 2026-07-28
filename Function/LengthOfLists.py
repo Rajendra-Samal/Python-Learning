45b7ec2d3ed12f166b19104f3fe1b29cb10b2b54
Rajendra Samal <rajendrakumar13863@gmail.com>
2026-07-28 11:13:31 +0530

    Added

---
+++ Function/LengthOfLists.py
@@ -0,0 +1,9 @@
+
+cities = ["delhi", "bhuban","banglore","Harudwar","kakati"]
+heroes = ["saktiman","spider","gulua","harihara"]
+
+def print_len(lists):
+  print(len(lists))
+
+print_len(cities)
+print_len(heroes)
